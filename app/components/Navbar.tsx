d5e1f932263d00d478351d99a2568f5aa5d32758
Subodhini0906 <subodhiniaggarwal@gmail.com>
2025-03-26 01:43:34 +0530

    style changes

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,10 +7,10 @@ const Navbar = async () => {
   const session = await auth();
 
   return (
-    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+    <header className="px-3 py-2 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
         <Link href="/">
-          <Image src="/logo.png" alt="logo" width={144} height={30} priority/>
+          <Image src="/logo.png" alt="logo" width={100} height={21} priority/>
         </Link>
 
         <div className="flex items-center gap-5 text-black">
