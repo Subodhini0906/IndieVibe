bb25b79e086d4e437eb2e5ced5f4a6eaa951a344
Subodhini0906 <subodhiniaggarwal@gmail.com>
2025-03-27 21:57:09 +0530

    added fncting

--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import Form from 'next/form'
+import SearchFormReset from './SearchFormReset';
 
 function SearchForm() {
+  const query='Test';
   return (
-    <Form action="/" scroll className="search-form"></Form>
+    <Form action="/" scroll={false} className="search-form">
+      <input name="query" placeholder='search nearby boutiques' defaultValue="" className='flex-1 font-bold placeholder:font-semibold placeholder:text-black-100 w-full h-auto outline-none'/>
+      <div className='flex gap-2'>
+        {query && <SearchFormReset/>}
+        <button type='submit' className='size-[50px] rounded-full bg-black flex justify-center items-center !important'>S</button>
+      </div>
+    </Form>
+
   )
 }
 
