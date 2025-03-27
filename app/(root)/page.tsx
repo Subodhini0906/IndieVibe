595a8967a10229ae995a10f596da3181a4d49996
Subodhini0906 <subodhiniaggarwal@gmail.com>
2025-03-27 19:30:45 +0530

    ...

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,17 @@
 export default function Home() {
   return (
     <>
-    <h1 className="w-screen bg-black text-white text-center py-5 text-2xl font-extrabold">HOME</h1>
+    <section className="w-full bg-indigo-100 min-h-[330px] flex justify-center items-center flex-col py-10 px-6">
+      <h1 className="w-full flex justify-center items-center bg-black text-white text-center py-5 text-3xl font-extrabold">
+        Dress with grace, 
+        <br/>
+        lead the race.
+      </h1>
+      <p className="font-medium mt-6 text-3xl text-black max-w-2xl text-center break-words">
+        Elevate Your Wardrobe, Elevate Your Career
+      </p>
+    </section>
     </>
   );
 }
+
