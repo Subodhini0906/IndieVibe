91225602c2d183f7cbe8984b5b9a7db449fb7631
Subodhini0906 <subodhiniaggarwal@gmail.com>
2025-04-14 22:08:51 +0530

    changes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,6 @@
-import { handlers } from "../../../../auth" // Referring to the auth.ts we just created
-export const { GET, POST } = handlers
+//import { handlers } from "../../../../auth"
+//export const { GET, POST } = handlers
+import { handlers } from "@/auth" // Import from wherever your auth.ts file is located
+// Export the handler functions directly
+export const GET = handlers.GET
+export const POST = handlers.POST
