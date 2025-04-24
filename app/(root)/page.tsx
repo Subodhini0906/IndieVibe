10f9f655b600fcdf8e16ab2deba62b1a0edd8bba
Subodhini0906 <subodhiniaggarwal@gmail.com>
2025-04-24 18:59:18 +0530

    add

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,31 +1,25 @@
-"use client"
-import SearchForm from "../components/SearchForm";
-import Image from "next/image";
+"use client";
 import SliderSection from "../components/SliderSection";
+
 export default function Page() {
   return (
     <>
-    <section className="relative w-full min-h-[330px] flex justify-center items-center flex-col py-10 px-6">
-      <Image 
-        src="/bg.png"
-        alt="Background"
-        fill
-        className="absolute inset-0 z-0 object-cover"
-        priority
-      />
-      <div className="relative z-20 w-full flex flex-col items-center">
-        <div className="w-full bg-white text-black text-center py-5 text-4xl font-extrabold">
-          Dress with grace, 
-          <br/>
-          lead the race.
-        </div>
-      <SearchForm/>
-      </div>
-    </section>
-    <section className="px-6 py-10 max-w-7xl mx-auto">
-      <SliderSection/>
-    </section>
+      <section className="relative w-full min-h-[330px] overflow-hidden">
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="absolute inset-0 z-0 w-full h-full object-cover"
+        >
+          <source src="/bg.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </section>
+
+      <section className="px-6 py-10 max-w-7xl mx-auto">
+        <SliderSection />
+      </section>
     </>
   );
 }
-
