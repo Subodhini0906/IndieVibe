1cd4fb6f99e995c8bcc6440442eb6c93c8810eea
Subodhini0906 <subodhiniaggarwal@gmail.com>
2025-05-06 20:10:07 +0530

    changes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,6 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import { NextAuthOptions } from "next-auth";
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/auth"; // your auth options config
 
-export const authOptions: NextAuthOptions ={
-  providers: [GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET?? '',
-  }),
-],
-}
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
